75df39cf4fab903d607b24d83d08a9ad6c9b0611
Hero Live <66154935+HeroLive@users.noreply.github.com>
2021-03-16 09:07:38 +0700

    Add files via upload

---
+++ 06_Active_buzzer/06_Active_buzzer.ino
@@ -0,0 +1,17 @@
+int buzzer = 12;//the pin of the active buzzer
+void setup()
+{
+ pinMode(buzzer,OUTPUT);//initialize the buzzer pin as an output
+}
+void loop()
+{
+   //output an frequency
+   for(int i=0;i<80;i++)
+   {
+    digitalWrite(buzzer,HIGH);
+    delay(1);//wait for 1ms
+    digitalWrite(buzzer,LOW);
+    delay(1);//wait for 1ms
+    }
+  delay(2000);
+} 
