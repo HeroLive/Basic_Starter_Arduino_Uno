302945ed5a8543f58130c52e523ac426f4966327
hnguyen376 <nqhung07@gmail.com>
2021-04-15 16:54:07 +0700

    no message

---
+++ 30_Relay/30_Relay.ino
@@ -0,0 +1,16 @@
+int RelayPin = 6;
+
+void setup() {
+  // Set RelayPin as an output pin
+  pinMode(RelayPin, OUTPUT);
+}
+
+void loop() {
+  // Let's turn on the relay...
+  digitalWrite(RelayPin, LOW);
+  delay(3000);
+  
+  // Let's turn off the relay...
+  digitalWrite(RelayPin, HIGH);
+  delay(3000);
+}
